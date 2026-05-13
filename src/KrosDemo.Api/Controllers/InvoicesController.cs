53c0341e15b7e992423aabb6bdb5225184cdef13
GP <gregor.pavlik@gmail.com>
2026-05-13 23:49:29 +0200

    delete

--- src/KrosDemo.Api/Controllers/InvoicesController.cs
+++ src/KrosDemo.Api/Controllers/InvoicesController.cs
@@ -28,6 +28,7 @@ public class InvoicesController : ControllerBase
         return Ok(data);
     }
 
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/ETag
     [Authorize(Roles = "Admin")]
     [HttpPut("{id:int}")]
     public async Task<ActionResult<InvoiceDTO>> UpdateInvoice(
